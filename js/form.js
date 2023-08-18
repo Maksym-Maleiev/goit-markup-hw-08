89c0b4fa81edf208a3a941df53a068482182864c
Maksym-Maleiev <maximmaleev93@gmail.com>
2023-08-18 14:48:20 +0300

    Create form.js

---
+++ js/form.js
@@ -0,0 +1,9 @@
+(() => {
+  document.querySelector('.js-speaker-form').addEventListener('submit', e => {
+    e.preventDefault();
+
+    new FormData(e.currentTarget).forEach((value, name) => console.log(`${name}: ${value}`));
+
+    e.currentTarget.reset();
+  });
+})();
